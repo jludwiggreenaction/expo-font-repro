e94bf0b9e78e083f514d091675c7dc80afa2ec45
Jackson Ludwig <jludwig@greenactionstudio.com>
2024-03-01 09:34:32 -0500

    add repro

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 export default function App() {
+  const [showOtherText, setShowOtherText] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      await sleep(15_000);
+      setShowOtherText(true);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontSize: 20 }}>This is some text.</Text>
+      {showOtherText && <Text style={{ fontFamily: 'Roboto', fontSize: 20 }}>This is some text.</Text>}
       <StatusBar style="auto" />
     </View>
   );
